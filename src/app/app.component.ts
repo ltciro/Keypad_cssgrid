ace84afc1a1c244421b2ed336c8e974d833f0342
Your Namefelixbanguera <jorge.banguera@yuxiglobal.com>
2018-10-24 15:46:46 -0500

    Added http request functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -12,23 +13,32 @@ export class AppComponent {
     {value:'1'},{value:'2'},{value:'3'},
     {value:'4'},{value:'5'},{value:'6'},
     {value:'7'},{value:'8'},{value:'9'},
-    {value:'back', icon:'back'},{value:'0'},{value:'delete', icon:'delete'}
-  ]
+    {value:'bkspace', icon:'back'},{value:'0'},{value:'cancel', icon:'delete'}
+  ];
+
+  constructor(private http: HttpClient) { }
+
   valPressed(key){
-    if(key==="back"){
+    if(key==="bkspace"){
       this.input = this.input.slice(0, - 1);
-      return;
-    }
-    if(key==="delete"){
+      // return;
+    }else if(key==="cancel"){
        this.input = '';
-       return;
+       // return;
+    }else{
+      this.input = this.input + key;
     }
-    this.input = this.input + key;
+    this.http.get('http://localhost:8000/send_input?user_input='+key).subscribe(data => {
+      console.log('hola Lau', data);
+    })
     console.log(this.input); // endpoint por letra
   }
 
   submit(){
+    this.http.get('http://localhost:8000/send_input?user_input=submit').subscribe(data => {
+      console.log('hola Lau submit', data);
+    })
     console.log(this.input); // endpoint por boton submit
   }
-  
+
 }
