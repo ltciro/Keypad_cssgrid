f43349c6cc21c949ba01d5e047bc15a279a72bff
“Hernan <hernan.sanchez@yuxipacific.com>
2018-10-25 10:01:58 -0500

    Added socketIOclient

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -33,7 +33,7 @@
 // import 'core-js/es6/map';
 // import 'core-js/es6/weak-map';
 // import 'core-js/es6/set';
-
+(window as any).global = window;
 /**
  * If the application will be indexed by Google Search, the following is required.
  * Googlebot uses a renderer based on Chrome 41.
