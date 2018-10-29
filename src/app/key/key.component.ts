17974abfcb8a8ddaa75f4320d61ccceb03b3f14b
“Hernan <hernan.sanchez@yuxipacific.com>
2018-10-29 14:33:43 -0500

    Dynamic_functions

--- src/app/key/key.component.ts
+++ src/app/key/key.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Key } from "src/app/key/key.interface";
-import { HOST } from '../configServer'
+import { Key } from 'src/app/key/key.interface';
+import { HOST } from '../configServer';
 
 @Component({
   selector: 'app-key',
@@ -11,18 +11,26 @@ import { HOST } from '../configServer'
 export class KeyComponent implements OnInit {
   @Input() key: Key ;
   @Output() valPressed = new EventEmitter<string>();
-
+  private setFunctions: String = 'a5f2d777.18b7c8';
+  private customizedNode: String = '68d88b02.e0ffe4';
+  private host = HOST;
   constructor(private http: HttpClient) {
 
    }
 
   ngOnInit() {
+    this.http.get(this.host + 'getNodeProp?nodeId=' + this.setFunctions).subscribe(data => {
+      Object.assign(this, data);
+      this.loadFuntions();
+    });
   }
 
-  keyPress(){
-    this.http.get(HOST + 'send_input?nodeId='+this.key[1]+'&user_input='+this.key[0]).subscribe(data => {
-      console.log('hola Lau', data);
-    })
+  loadFuntions() {
+    console.log(this['json']);
+    let global_this = this;
+    JSON.parse(this['json']).forEach(function(f_info) {
+      let function_definition = new Function(f_info.params, f_info.logic);
+      global_this[f_info.name] = function_definition;
+    });
   }
-
 }
